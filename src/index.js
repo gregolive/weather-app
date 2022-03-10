60575d399c8245b67f30a2f4145b4b0f9c8dd336
Gregory Olive <gregoryolive@outlook.com>
2022-03-10 23:44:43 +0900

    Switch event listener from button click to form submit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import '@fortawesome/fontawesome-free/js/all';
 import queryWeatherData from './queryWeatherData';
 import displaySearch from './displaySearch';
 
-const searchWeather = async () => {
+const searchWeather = async (e) => {
+  e.preventDefault();
   const query = document.querySelector('.search-input').value;
   const results = await queryWeatherData(query);
   displaySearch(query, results);
 };
 
-document.querySelector('.search-btn').addEventListener('click', searchWeather);
+document.querySelector('.search-form').addEventListener('submit', searchWeather);
