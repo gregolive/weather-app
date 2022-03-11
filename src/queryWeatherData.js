f36bd5b35d4bc812b2e0c288bb37d2454337ea06
Gregory Olive <gregoryolive@outlook.com>
2022-03-12 02:01:00 +0900

    Update query to https

--- src/queryWeatherData.js
+++ src/queryWeatherData.js
@@ -1,5 +1,5 @@
 const queryWeatherData = async (query) => {
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${query}&APPID=111fdbb7d75b9450d8175d0fb69e49cc`, { mode: 'cors' });
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&APPID=111fdbb7d75b9450d8175d0fb69e49cc`, { mode: 'cors' });
   return response.json();
 };
 
